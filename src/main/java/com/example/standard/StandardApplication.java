7a0d5abb913870d01284170e5ef2bd6f74d5b2d5
joon448 <joonjdy448@gmail.com>
2025-09-03 09:28:42 +0900

    Chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/standard/StandardApplication.java
@@ -0,0 +1,17 @@
+package com.example.standard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class StandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardApplication.class, args);
+	}
+
+}
