eb1182dd3f96b3a07cae1bacc78c0259d4cc28be
joon448 <joonjdy448@gmail.com>
2025-09-03 09:31:19 +0900

    Feat: Product Entity 생성

---
+++ src/main/java/com/example/standard/product/entity/Product.java
@@ -0,0 +1,29 @@
+package com.example.standard.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@Table(name = "products")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+}
